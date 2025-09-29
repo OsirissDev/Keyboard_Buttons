f0d2a2d0a047d7113600be9412cd7c7f67237981
Osiris <bryanbulford@bryans-imac.mynetworksettings.com>
2025-09-29 16:55:36 -0500

    main keyboard layout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-pygame.init()
 
+pygame.init()
+clock = pygame.time.Clock()
 class button(pygame.sprite.Sprite):
     def __init__(self, key, dimensions, coordinates):
         super().__init__()
@@ -9,21 +10,38 @@ class button(pygame.sprite.Sprite):
         self.rect= pygame.Rect(self.coordinates[0], self.coordinates[1], dimensions[0], dimensions[1])
         self.color = "white"
 
+
     def pressed(self, bool):
         if bool:
             self.color = 'white'
         else: self.color = 'black'
 
     def draw(self, screen):
-        pygame.draw.rect(screen, self.color, self.rect)
-        print("drawn")
+        pygame.draw.rect(screen, self.color, self.rect, 2)
+        key_text = font.render(self.key, False, "white")
+        key_text_rect = key_text.get_rect(center = self.rect.center)
+        screen.blit(key_text, key_text_rect)
 
-scale=50
 
-screen = pygame.display.set_mode((13*scale, 6*scale))
+scale=150
+font = pygame.font.SysFont("Sans-serif", 1*scale//2)
 
-test=button("Q",(1*scale, 1*scale), (3*scale, 6*scale))
+screen = pygame.display.set_mode((13*scale, 6*scale), pygame.SCALED, vsync=1)
 
+button_list=pygame.sprite.Group()
+
+characters= ["~1234567890-=" , "qwertyuiop[]\\", "asdfghjkl;'" , "zxcvbnm,./"]
+
+# creating button classes
+for y in range(len(characters)):
+    for i in range(len(characters[y])):
+        x_coord_base= (((i/2)+1) * scale) #variables for readability while finding the x coord
+        x_coordinate = (x_coord_base + i *( .25 * scale)) + .25*scale
+
+        y_coord_base = (((y / 2) + 1) * scale)  # variables for readability while finding the x coord
+        y_coordinate = (y_coord_base + y * (.25 * scale)) + .25 * scale
+
+        button_list.add(button(characters[y][i], (.5 * scale, .5 * scale), (x_coordinate, y_coordinate)))
 
 while True:
     for event in pygame.event.get():
@@ -34,5 +52,9 @@ while True:
 
     pygame.draw.rect(screen, 'white', pygame.Rect(1*scale, 1*scale, 11*scale, 4*scale), 4)
 
-    test.draw(screen)
+    for b in button_list:
+        b.draw(screen)
     pygame.display.update()
+
+    clock.tick(30)
+    # print(clock.get_fps())
