2f7e14535f4ee880e0239c4a084795a69c56d191
Osiris <bryanbulford@bryans-imac.mynetworksettings.com>
2025-09-29 13:37:36 -0500

    commit

--- main.py
+++ main.py
@@ -2,14 +2,27 @@ import pygame
 pygame.init()
 
 class button(pygame.sprite.Sprite):
-    def __init__(self, button, coordinates):
+    def __init__(self, key, dimensions, coordinates):
         super().__init__()
-        self.button = button
+        self.key = key
         self.coordinates = coordinates
+        self.rect= pygame.Rect(self.coordinates[0], self.coordinates[1], dimensions[0], dimensions[1])
+        self.color = "white"
 
+    def pressed(self, bool):
+        if bool:
+            self.color = 'white'
+        else: self.color = 'black'
 
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect)
+        print("drawn")
 
-screen = pygame.display.set_mode((13*50, 6*50), pygame.SCALED, vsync=1)
+scale=50
+
+screen = pygame.display.set_mode((13*scale, 6*scale))
+
+test=button("Q",(1*scale, 1*scale), (3*scale, 6*scale))
 
 
 while True:
@@ -17,8 +30,9 @@ while True:
         if event.type == pygame.QUIT: pygame.quit(), exit()
 
         if event.type == pygame.KEYDOWN:
-            pass
+            print(event.key)
 
-    pygame.draw.rect(screen, 'white', pygame.Rect(1*50, 1*50, 11*50, 4*50))
+    pygame.draw.rect(screen, 'white', pygame.Rect(1*scale, 1*scale, 11*scale, 4*scale), 4)
 
+    test.draw(screen)
     pygame.display.update()
